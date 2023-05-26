103cd5091c2b9f7e375646cc950712171e029fdb
L0R3NZ098 <lorenzo.cennamo98@gmail.com>
2023-05-26 16:58:50 +0200

    aggiornamento 26/05/2023

--- jsjava.js
+++ jsjava.js
@@ -310,38 +310,373 @@ let risultato = addPositive(numbers);
 console.log(risultato);
 */
 
-// Esercitazione 20/05/2023
+//Esercizio 4
 
-// Esercizio 1
+/*
+const prodotti = ["Smartphone", "Auricolari", "Bluetooth", "Motorino", "Auto", "T-shirt", "Felpa con cappuccio", "Nano da giardino", "Videogioco Super Nintendo"];
+
+
+function find(prodotti) {
+    for(let i=0; i<prodotti.length; i++) {
+        if(prodotti[i].toLowerCase() === prodotti.toLowerCase()) {
+            return `trovato!`;
+        } 
+    };
+    return `non trovato!`;
+}
+
+let result = "";
+
+do {
+    let sceltaUtente = prompt(`quale prodotto cerchi?`);
+    result = find(sceltaUtente);
+    console.log(result);
+} while (result === `non trovato!`);
+*/
+
+//Esercizio 6
+
+/*
+let frase = `Ciao sono un esercizio sui metodi degli array`
+
+function checkFirstLetters(testo) {
+    let nuovoArray = [];
+    const togliSpazi = testo.split(" ");
+    for(let i=0; i < togliSpazi.length; i++) {
+        nuovoArray.push(togliSpazi[i].charAt(0))
+    }
+    let fraseFinale = nuovoArray.join("");
+    return fraseFinale;
+}
+console.log(checkFirstLetters(frase));
+*/
+
+//Esercitazione 19/05/2023
+
+//Esercizio 1
 
 /*
 let playlist = {
     canzoni: [
-  
-    ]
+        {
+            titolo: "",
+            autore: "",
+            anno: ""
+        },
+        {
+            titolo: "",
+            autore: "",
+            anno: "",
+        },
+    ],
+    aggiungi: function(titolo, autore, anno, index) {
+        this.canzoni[index].titolo = titolo;
+        this.canzoni[index].autore = autore;
+        this.canzoni[index].anno = anno;
+    }
 }
 
-let canzone1 = {
-    titolo: "",
-    nomeCantante: "",
-    anno: ""
-};
+playlist.aggiungi("Con calma", "Daddy Yankee", "2020", 0);
+playlist.aggiungi("Narcos", "Geolier", "2023", 1);
 
-let canzone2 = {
-    titolo: "",
-    nomeCantante: "",
-    anno: ""
-};
-
-canzone1.titolo = "amore amaro";
-canzone1.nomeCantante = "gigi finizio";
-canzone1.anno = "2000";
-
-canzone2.titolo = "allenamento 2";
-canzone2.nomeCantante = "capo plaza";
-canzone2.anno = "2016";
-
-playlist.canzoni.push(canzone1, canzone2);
-
-console.log(playlist);
+playlist.canzoni.forEach(canzone => {
+    console.log(`Titolo: ${canzone.titolo}, Autore: ${canzone.autore}, Anno: ${canzone.anno}`);
+});
 */
+
+//Esercizio 2
+
+/*
+let rubrica = {
+    utenti: [
+        utente1 = {
+            nome: "angelo",
+            cognome: "spinillo",
+            telefono: "43167589486",
+            indirizzo: {
+                via: "via scarlatta",
+                citta: "Napoli",
+                provincia: "NA"
+            }
+        },
+        utente2 = {
+            nome: "pierpaolo",
+            cognome: "vattelapesca",
+            telefono: "34056791864",
+            indirizzo: {
+                via: "via delle pigne",
+                citta: "Milano",
+                provincia: "MI"
+            }
+        },
+        utente3 = {
+            nome: "davide",
+            cognome: "ambrosini",
+            telefono: "4978512654",
+            indirizzo: {
+                via: "via epomeo",
+                citta: "Roma",
+                provincia: "RM"
+            }
+        },
+        utente4 = {
+            nome: "mario",
+            cognome: "rossi",
+            telefono: "6789451234",
+            indirizzo: {
+                via: "via fasulla 123",
+                citta: "Springfield",
+                provincia: "SP"
+            }
+        }
+    ],
+    informazioniUtente: function () {
+        rubrica.utenti.forEach(contatto => {
+            console.log(`Nome: ${contatto.nome}, Cognome: ${contatto.cognome}, Telefono: ${contatto.telefono}`)
+        });
+    }
+};
+
+// rubrica.informazioniUtente();
+
+rubrica.utenti.pop();
+
+rubrica.informazioniUtente();
+
+let nomiRubrica = (rubrica) =>  {
+    console.log(`Gli utenti della rubrica sono:`)
+    rubrica.forEach(utente => {
+        console.log(`${utente.nome}`)
+    })
+}
+
+nomiRubrica(rubrica.utenti);
+*/
+
+//Esercizio 3
+
+/*
+let garage = {
+    auto: [
+        {
+            nome: "qashqai",
+            marca: "nissan"
+        },
+        {
+            nome: "model x",
+            marca: "tesla"
+        },
+        {
+            nome: "ID. 4",
+            marca: "volkswagen"
+        },
+        {
+            nome: "spring",
+            marca: "dacia"
+        }
+    ],
+    infoVeicoli: function()  {
+        this.auto.forEach(vettura => {
+            console.log(` ${vettura.marca} - ${vettura.nome} `)
+        })
+    }
+}
+
+garage.infoVeicoli();
+
+let macchine = (garage) => {
+    console.log(`i modelli sono:\n`);
+    garage.forEach(pippo => {
+        console.log(`${pippo.nome}`);
+    })
+}
+
+macchine(garage.auto);
+*/
+
+//Esercizio 4
+
+/*
+const libri = [
+    {
+        titolo: "Harry Potter",
+        autore: "Rowling",
+        categoria: "fantasy",
+    },
+    {
+        titolo: "Il piccolo principe",
+        autore: "Exuperie",
+        categoria: "narrativa classica"
+    },
+    {
+        titolo: "Il Signore degli Anelli",
+        autore: "Tolkien",
+        categoria: "fantasy"
+    },
+    {
+        titolo: "Le avventure di Tom Sawyer",
+        autore: "Mark",
+        categoria: "narrativa classica"
+    },
+    {
+        titolo: "Percy Jackson e il ladro di fulmini",
+        autore: "Rick",
+        categoria: "fantasy"
+    }
+];
+
+let ricerca = prompt(`inserisci una categoria tra: "fantasy" e "narrativa classica"`);
+
+function cercaLibro(keyWord) {
+    let result = libri.filter(libro => libro.categoria.toLowerCase().includes(keyWord.toLowerCase()));
+    if (result.length === 0) {
+        console.log(`Non ho trovato nessun libro che abbia quel genere`);
+    }
+    result.forEach(book => {
+        console.log(`<a href="www.lamiasuperlibreria.aulab/${book.autore.toLowerCase()}/${book.titolo.toLowerCase().replace(/ /g, "-")}.html">${book.titolo.toLowerCase().replace(/ /g, "-")}</a>`);
+    })
+};
+
+console.log(cercaLibro(ricerca));
+*/
+
+//Esercitazione 23/05/2023
+
+//Esercizio1
+
+/*
+const library = [ 
+    {
+        title: 'Bill Gates',
+        author: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        title: 'Steve Jobs',
+        author: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        author: 'Suzanne Collins',
+        readingStatus: false
+    }
+];
+
+
+
+function letto(libreria) {
+    libreria.forEach(book => {
+        if(book.readingStatus === true) {
+            console.log(`il libro ${book.title} di ${book.author} è stato letto`)
+            return;
+        } 
+        console.log(`il libro ${book.title} di ${book.author} non è stato letto`)
+    })
+}
+
+letto(library);
+*/
+
+//Esercizio 2
+
+/*
+const bowling = {
+    players: [
+        { name: "Andrea", scores: [10, 7, 9, 7, 6, 10, 10, 9, 10, 8, 7, 10, 10, 10, 9, 8, 10] },
+        { name: "Marco", scores: [9, 8, 10, 10, 7, 10, 10, 10, 9, 10, 8, 7, 10, 10, 10, 9, 10] },
+        { name: "Davide", scores: [10, 10, 7, 8, 10, 7, 9, 8, 10, 10, 10, 9, 8, 10, 10, 10, 7] }
+    ]
+};
+
+function playerTotals(bowling) {
+    let giocatori = bowling.players;
+    giocatori.forEach(giocatore => {
+        let punti = giocatore.scores;
+        let contatore = 0;
+        punti.forEach(punto => {
+            contatore += parseInt(punto)
+        })
+        console.log(contatore);
+        giocatore.total = contatore;
+    });
+}
+
+function checkWinner(bowling) {
+    let giocatori = bowling.players;
+    let totale = giocatori.map(player => player.total);
+    let punteggioAlto = Math.max(...totale)
+    console.log(punteggioAlto)
+    giocatori.forEach(giocatore => {
+        if (giocatore.total === punteggioAlto) {
+            console.log(`Nome: ${giocatore.name}, punteggio: ${giocatore.scores}, totale: ${giocatore.total}`)
+        }
+    })
+}
+
+playerTotals(bowling)
+checkWinner(bowling)
+*/
+
+//Esercizio 3
+
+/*
+let calendario = {
+    lunedi: [],
+    martedi: [],
+    mercoledi: [],
+    giovedi: [],
+    venerdi: [],
+    sabato: [],
+    domenica: []
+}
+
+class Evento {
+    constructor(nomeEvento, inizioEvento) {
+        this.nomeEvento = nomeEvento;
+        this.inizioEvento = inizioEvento;
+    }
+}
+
+function inserisci (evento, giorno) {
+    calendario[giorno].push(evento);
+}
+
+function orderAppointments (calendario, giorno) {
+    calendario[giorno].sort(function(a, b){
+            return a["inizioEvento"].replace(/:/g, '') - b["inizioEvento"].replace(/:/g, '');
+          });
+    console.log(calendario)
+    
+}
+
+function cercaAppuntamenti(giorno) {
+    calendario[giorno].forEach(appuntamento => {
+        console.log(appuntamento)
+    });
+}
+
+let evento1 = new Evento("lezione aulab", "ore 18.30");
+let evento3 = new Evento("lezione aulab", "ore 18.30");
+let evento5 = new Evento("lezione aulab", "ore 18.30");
+let evento2= new Evento("esercitazione aulab", "ore 19");
+let evento4= new Evento("esercitazione aulab", "ore 19");
+
+inserisci(evento1, "lunedi");
+inserisci(evento3, "mercoledi");
+inserisci(evento5, "venerdi");
+inserisci(evento2, "martedi");
+inserisci(evento4, "giovedi"); 
+
+orderAppointments(calendario, "lunedi");
+
+let cerca = "lunedi";
+
+let giorni = Object.keys(calendario);
+
+let giornoTrovato = giorni.filter(giorno => giorno === cerca);
+
+console.log(giornoTrovato)
+
+cercaAppuntamenti(giornoTrovato);
+/*
