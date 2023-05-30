2db4241208f68cd70d8ff46bf81a6d9b2b62fbfc
L0R3NZ098 <lorenzo.cennamo98@gmail.com>
2023-05-30 22:24:04 +0200

    aggiornamento 30/05/2023

---
+++ categories.js
@@ -0,0 +1,14 @@
+const categories = [
+    {
+        text: "Fantasy",
+        url: "#",
+    },
+    {
+        text: "SuperHero",
+        url: "#",
+    },
+    {
+        text: "Manga",
+        url: "#",
+    },
+];
